cd84513f3f381f35f4777b9cfa1b1cc333c5d0e7
Benny Chui <bennychui38@gmail.com>
2023-06-07 03:17:16 +0800

    7-jun-2023 page test

---
+++ assets/script.js
@@ -0,0 +1,65 @@
+function copyMenu() {
+    var dptCategory = document.querySelector('.dpt-cat');
+    var dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+    var mainNav = document.querySelector('.header-nav nav');
+    var navPlace = document.querySelector('.off-canvas nav')
+    navPlace.innerHTML = mainNav.innerHTML;
+
+    var topNav = document.querySelector('.header-top');
+    var topPlace = document.querySelector('.off-canvas .thetop-nav')
+    topPlace.innerHTML = topNav.innerHTML;
+}
+copyMenu();
+
+const menuButton = document.querySelector('.trigger'),
+    closeButton = document.querySelector('.t-close'),
+    addclass = document.querySelector('.site');
+menuButton.addEventListener('click',function() {
+    addclass.classList.toggle('showmenu')
+})
+closeButton.addEventListener('click' , function() {
+    addclass.classList.remove('showmenu')
+})
+
+
+const submenu = document.querySelectorAll('.has-child .icon-small');
+submenu.forEach((menu) => menu.addEventListener('click',toggle));
+
+function toggle(e){
+    e.preventDefault();
+    submenu.forEach((itme) => itme != this ? itme.closest('.has-child').classList.remove('expand') : null);
+    if (this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand')
+}
+
+
+// Get all slide elements
+var slides = document.getElementsByClassName('slide');
+
+// Set initial slide index
+var currentSlideIndex = 0;
+
+// Show the first slide
+slides[currentSlideIndex].classList.add('active');
+
+// Function to show next slide
+function nextSlide() {
+  // Hide the current slide
+  slides[currentSlideIndex].classList.remove('active');
+
+  // Increment the slide index
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+
+  // Show the next slide
+  slides[currentSlideIndex].classList.add('active');
+}
+
+// Set interval to automatically switch slides every 5 seconds
+setInterval(nextSlide, 5000);
+
+
+// const year = new Date().getFullYear();
+// const copyright = document.querySelector('footer p');
+// copyright.innerHTML = `&copy; ${year} Your Website. All rights reserved.`;
